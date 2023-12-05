255d2921127812c2111d438b2b6f4257fd793eab
delianvalchanov <97537500+delianvalchanov@users.noreply.github.com>
2023-12-05 16:02:02 +0200

    Sign in/ Sign up modal template
    Setting up login/register modal

--- my-app/src/components/Header/SignInModal.js
+++ my-app/src/components/Header/SignInModal.js
@@ -1,69 +1,153 @@
 import {
-    Button,
-    Container,
-    Form,
-    FormLabel,
-    InputGroup,
-    Modal,
-    ButtonGroup
+  Button,
+  Container,
+  Form,
+  FormLabel,
+  InputGroup,
+  Modal,
+  ButtonGroup,
+  Tab,
+  Tabs,
 } from "react-bootstrap";
-import HeaderCSS from './Header.module.css'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import HeaderCSS from "./Header.module.css";
 
 export const SignInModal = ({ show, toggleShow, successfullSubmission }) => {
-
-    return (
-        <Modal className={HeaderCSS.wrapper} show={show} toggleShow={toggleShow}>
-            <Modal.Header>
-                <Modal.Title>Sign in</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form
-                    method="POST"
-                    autoComplete="on"
-                    id={HeaderCSS['login']}
+  return (
+    <Modal
+      className={`modal-dialog-centered ${HeaderCSS.wrapper}`}
+      show={show}
+      onHide={toggleShow}
+    >
+      <Modal.Body>
+        <Tabs
+          className="mb-3 nav-fill"
+          id="uncontrolled-tab-example"
+          defaultActiveKey="signIn"
+        >
+          <Tab className={HeaderCSS.tab} eventKey="signIn" title="Sign in">
+            <Form method="POST" autoComplete="on" id={HeaderCSS["login"]}>
+              <Container className="container">
+                <p className={HeaderCSS.optionsDesc}>
+                  with your social network
+                </p>
+                <ButtonGroup className={HeaderCSS.socialButtons}>
+                  <button
+                    className={HeaderCSS.social}
+                    id={HeaderCSS["facebook"]}
+                  >
+                    {" "}
+                    <FontAwesomeIcon
+                      className={`fa-3x ${HeaderCSS.facebookIcon}`}
+                      icon={faFacebook}
+                    />{" "}
+                  </button>
+                  <button className={HeaderCSS.social} id={HeaderCSS["google"]}>
+                    {" "}
+                    <FontAwesomeIcon
+                      className={`fa-3x ${HeaderCSS.googleIcon}`}
+                      icon={faGoogle}
+                    />{" "}
+                  </button>
+                  <button className={HeaderCSS.social} id={HeaderCSS["apple"]}>
+                    {" "}
+                    <FontAwesomeIcon
+                      className={`fa-3x ${HeaderCSS.twitterIcon}`}
+                      icon={faTwitter}
+                    />{" "}
+                  </button>
+                </ButtonGroup>
+                <p className={HeaderCSS.optionsDesc}>or</p>
+                <InputGroup className="mb-2">
+                  <FormLabel htmlFor="email">Email: </FormLabel>
+                  <input
+                    className={HeaderCSS.authField}
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="peter@gmail.com"
+                  />
+                </InputGroup>
+                <InputGroup className="mb-1">
+                  <FormLabel htmlFor="login-pass">Password: </FormLabel>
+                  <input
+                    className={HeaderCSS.authField}
+                    type="password"
+                    name="password"
+                    id="password"
+                  />
+                </InputGroup>
+                <Button
+                  type="submit"
+                  className={`w-100 mb-2 ${HeaderCSS.submit}`}
+                  value="Submit"
                 >
-                    <Container className="container">
-                        <ButtonGroup className={HeaderCSS.socialButtons}>
-                            <button className={HeaderCSS.social} id={HeaderCSS['facebook']}></button>
-                            <button className={HeaderCSS.social} id={HeaderCSS['google']}></button>
-                            <button className={HeaderCSS.social} id={HeaderCSS['apple']}></button>
-                        </ButtonGroup>
-                        <InputGroup>
-                            <FormLabel htmlFor="email">Email: </FormLabel>
-                            <input
-                                className={HeaderCSS.authField}
-                                type="email"
-                                name="email"
-                                id="email"
-                                placeholder="peter@gmail.com"
-                            />
-                        </InputGroup>
-                        <fieldset>
-                            <label htmlFor="login-pass">Password: </label>
-                            <input
-                                className={HeaderCSS.authField}
-
-                                type="password"
-                                name="password"
-                                id="password"
-                            />
-                        </fieldset>
-                        <Button
-                            type="submit"
-                            className={`w-100 ${HeaderCSS.submit}`}
-                            value="Submit"
-                        >
-                            Login
-                        </Button>
-                        <p>
-                            <span>
-                                If you don't have profile, click <a href="#">here</a>
-                            </span>
-                        </p>
-                    </Container>
-                </Form>
-            </Modal.Body>
-        </Modal>
-    );
+                  Login
+                </Button>
+                <p>
+                  <span>
+                    If you don't have profile, click <a href="#">here</a>
+                  </span>
+                </p>
+              </Container>
+            </Form>
+          </Tab>
+          <Tab className={HeaderCSS.tab} eventKey="signUp" title="Sign up">
+            <Form
+              method="POST"
+              autoComplete="off"
+              className="pt-4"
+              id={HeaderCSS["login"]}
+            >
+              <Container className="container">
+                <InputGroup className="mb-2">
+                  <FormLabel htmlFor="email">Email: </FormLabel>
+                  <input
+                    className={HeaderCSS.authField}
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="peter@gmail.com"
+                  />
+                </InputGroup>
+                <InputGroup className="mb-2">
+                  <FormLabel htmlFor="login-pass">Password: </FormLabel>
+                  <input
+                    className={HeaderCSS.authField}
+                    type="password"
+                    name="password"
+                    id="password"
+                  />
+                </InputGroup>
+                <InputGroup className="mb-1">
+                  <FormLabel htmlFor="login-pass">Repeat password: </FormLabel>
+                  <input
+                    className={HeaderCSS.authField}
+                    type="password"
+                    name="repeat-password"
+                    id="repeat-password"
+                  />
+                </InputGroup>
+                <Button
+                  type="submit"
+                  className={`w-100 ${HeaderCSS.submit}`}
+                  value="Submit"
+                >
+                  Register
+                </Button>
+                <p>
+                  <span>
+                    If you have profile, click <a href="#">here</a>
+                  </span>
+                </p>
+              </Container>
+            </Form>
+          </Tab>
+        </Tabs>
+      </Modal.Body>
+    </Modal>
+  );
 };
--- my-app/src/components/Services/Services.js
+++ my-app/src/components/Services/Services.js
@@ -1,46 +1,76 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBowlFood } from "@fortawesome/free-solid-svg-icons";
+import { faFishFins } from "@fortawesome/free-solid-svg-icons";
+import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { faHandshake } from "@fortawesome/free-solid-svg-icons";
+
 export const Services = () => {
-    return (
-        <div className="container-xxl py-5">
-            <div className="container">
-                <div className="row g-4">
-                    <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
-                        <div className="service-item rounded pt-3">
-                            <div className="p-4">
-                                <i className="fa fa-3x fa-user-tie text-primary mb-4" />
-                                <h5>Master Chefs</h5>
-                                <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
-                        <div className="service-item rounded pt-3">
-                            <div className="p-4">
-                                <i className="fa fa-3x fa-utensils text-primary mb-4" />
-                                <h5>Quality Food</h5>
-                                <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
-                        <div className="service-item rounded pt-3">
-                            <div className="p-4">
-                                <i className="fa fa-3x fa-cart-plus text-primary mb-4" />
-                                <h5>Online Order</h5>
-                                <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
-                        <div className="service-item rounded pt-3">
-                            <div className="p-4">
-                                <i className="fa fa-3x fa-headset text-primary mb-4" />
-                                <h5>24/7 Service</h5>
-                                <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="container-xxl py-5">
+      <div className="container">
+        <div className="row g-4">
+          <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
+            <div className="service-item rounded pt-3">
+              <div className="p-4">
+                <FontAwesomeIcon
+                  className="fa-3x text-primary mb-4"
+                  icon={faBowlFood}
+                />
+                <h5>Healthy recipes</h5>
+                <p>
+                  Try our easy and helthy recipes, ready for minutes and amazing
+                  taste.
+                </p>
+              </div>
             </div>
+          </div>
+          <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
+            <div className="service-item rounded pt-3">
+              <div className="p-4">
+                <FontAwesomeIcon
+                  className="fa-3x text-primary mb-4"
+                  icon={faFishFins}
+                />{" "}
+                <h5>Quality Ingredients</h5>
+                <p>
+                  Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita
+                  amet diam
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
+            <div className="service-item rounded pt-3">
+              <div className="p-4">
+                <FontAwesomeIcon
+                  className="fa-3x text-primary mb-4"
+                  icon={faCartArrowDown}
+                />{" "}
+                <h5>Online Order</h5>
+                <p>
+                  Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita
+                  amet diam
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
+            <div className="service-item rounded pt-3">
+              <div className="p-4">
+                <FontAwesomeIcon
+                  className="fa-3x text-primary mb-4"
+                  icon={faHandshake}
+                />{" "}
+                <h5>Free Consultation</h5>
+                <p>
+                  Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita
+                  amet diam
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
